22c31c696fe90ddae79c6b61b18f5da44fade9fa
ahlev <ohahlev@gmail.com>
2020-06-02 23:47:15 +0700

    update

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup, find_packages
 
-__version__ = '0.0.3'
+__version__ = '0.0.4'
 
 setup(
     name='ahlev_django_brand',
@@ -15,8 +15,8 @@ setup(
     url='https://github.com/ohahlev/ahlev_django_brand',
     packages=find_packages(),
     install_requires=[
-        'ahlev_django_uikit',
-        'Pillow'
+        'Pillow',
+        'ahlev_django_layout'
     ],
     classifiers=[
         'Development Status :: 4 - Beta',
